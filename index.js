2593c6e8db58d3a1f5793b51092d46cc80e874b3
absam-bc <abdisalanahmed95@gmail.com>
2026-06-30 12:34:53 +0300

    done

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
+const personalInfo={
+  username:"user1",
+  password:"password123",
+}
 
+function createLoginTracker(personalInfo){
+  let attemptCount=0;
+  locked=false;
+  let loginTracker=(passwordAttempt)=>{
+    if(locked){
+      console.log("Account locked")
+      return
+    }
+    if(passwordAttempt===personalInfo.password){
+      console.log("Login successful");
+    }else {
+      attemptCount++;
+    }
+    if(attemptCount>=3){
+      let locked=true;
+    }
+  }
+  return loginTracker
+}
 
+let login=createLoginTracker(personalInfo);
+
+login("wrong");
+login("wrong");
+login("password123");
 
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
